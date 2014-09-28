e066cdf00697d1c27a226735508356db7d420c4e
JohnyMoSwag <johnymoswag@gmail.com>
2014-09-27 18:20:54 -0700

    update client test

--- tests/test_client.py
+++ tests/test_client.py
@@ -51,8 +51,8 @@ def test_bad_pub_key():
 def test_check_version():
     config = TConfig()
     client = Client(config, test=True)
-    assert client.update_check(client.app_name, '0.0.1') is True
     assert client.update_check(client.app_name, '6.0.0') is False
+    assert client.update_check(client.app_name, '0.0.1') is True
 
 
 @with_setup(None, tear_down)
